654707caa7a1627284dd9da9c4f67b07e28176a4
Simon Dablander <sdabland@student.42vienna.com>
2024-10-16 18:25:48 +0200

    fix(ex01): wrong `attack_damage` for `ScavTrap`

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -8,7 +8,7 @@ ScavTrap::ScavTrap() {
   name_ = "Anonymous ScavTrap";
   hit_points_ = 100;
   energy_points_ = 50;
-  attack_damage_ = 50;
+  attack_damage_ = 20;
 }
 
 ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name) {
@@ -16,7 +16,7 @@ ScavTrap::ScavTrap(const std::string &name) : ClapTrap(name) {
 
   hit_points_ = 100;
   energy_points_ = 50;
-  attack_damage_ = 50;
+  attack_damage_ = 20;
 }
 
 ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other) {
